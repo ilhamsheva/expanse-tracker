40c76e2f2ed28d5780f56793584fb3f40e75d981
ilhamsheva <ilhamshv@gmail.com>
2025-11-09 20:51:08 +0700

    fix(controller): Fix syntax on dashboardController and dashboardRoutes

--- backend/routes/dashboardRoutes.js
+++ backend/routes/dashboardRoutes.js
@@ -1,5 +1,7 @@
 import express from 'express';
 import { protect } from '../middleware/middleware.js';
+import { getDashboardData } from '../controllers/dashboardController.js';
+
 
 const router = express.Router();
 
